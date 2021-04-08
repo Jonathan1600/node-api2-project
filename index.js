f317e0290187f5792d8e4497ed5caa1fc9518492
unknown <jonathancalderonsilberman@gmail.com>
2021-04-08 14:08:25 -0700

    dotenv

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // require your server and launch it here
+const dotenv = require("dotenv").config();
 const server = require("./api/server");
+const port = process.env.PORT || 9000
 
-server.listen(3001, () => {
-    console.log('\n*** Server Running on http://localhost:3001 ***\n');
+server.listen(port, () => {
+    console.log(`\n*** Server Running on ${port} ***\n`);
 });
